236f3e4decd73939800ea52c9d383376e937b03a
CybransLoyalist <nendorel@gazeta.pl>
2017-12-12 19:31:04 +0100

    skonczenie - przed refaktorem. dodanie formularza kontaktowego

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -9,19 +9,26 @@ export default class ContactMe extends Component {
       <section id="contactMe" >
       <div className="container text-light">
         <h1 className="display-3">Contact me</h1>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-        <p>contact me</p>
-      </div>
+        <div className="contactBox p-3">
+          <form>
+            <div className="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control"/>
+            </div>
+            <div className="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control"/>
+            </div>
+            <div className="form-group">
+              <label for="message">Message</label>
+              <textarea className="form-control"></textarea>
+            </div>
+            <button className="btn btn-dark btn-block">
+            Send
+          </button>
+          </form>
+          </div>
+        </div>
     </section>
     </div>
     );
