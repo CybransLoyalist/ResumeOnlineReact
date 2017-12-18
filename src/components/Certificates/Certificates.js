85d6c93a31f59a1f0c284744c91eb186e132c191
CybransLoyalist <nendorel@gazeta.pl>
2017-12-18 17:32:09 +0100

    dodanie certyfikatu z webpacka

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -12,6 +12,7 @@ export default class Certificates extends Component {
           <div className="m-2 py-2">
             <Certificate img="src/img/udemyLogo.png" text="Modern React with Redux" href="https://www.udemy.com/certificate/UC-C4GRKXHL/"/>
             <Certificate img="src/img/udemyLogo.png" text="Bootstrap 4 From Scratch With 5 Projects" href="https://www.udemy.com/certificate/UC-1F56TODI/"/>
+            <Certificate img="src/img/udemyLogo.png" text="Webpack 2: The Complete Developer's Guide" href="https://www.udemy.com/certificate/UC-IL8GX7LK/"/>
           </div>
         </div>
       </section>
